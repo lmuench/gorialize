3c06a4cd5755ef2f053b0f5063e8191c410073bf
lmuench <muench.ludwig@gmail.com>
2019-08-25 17:10:40 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// type IDB interface {
+// 	Insert(item interface{}) uint
+// 	Get(id uint) interface{}
+// 	Update(id uint, item interface{})
+// 	Delete(id uint)
+// }
+
+type DB struct {
+	Tables map[string]Table
+}
+
+func newDB() DB {
+	return DB{
+		Tables: make(map[string]Table),
+	}
+}
+
+type Table []interface{}
+
+func (db *DB) Insert(resource interface{}) uint {
+	model := reflect.TypeOf(resource).String()
+	db.Tables[model] = append(db.Tables[model], resource)
+	return uint(len(db.Tables[model]))
+}
+
+func (db *DB) Get(id uint, model interface{}) interface{} {
+	return db.Tables[reflect.TypeOf(model).String()][id]
+}
+
+func (db *DB) Update(id uint, resource interface{}) {
+	model := reflect.TypeOf(resource).String()
+	db.Tables[model][id] = resource
+}
+
+type User struct {
+	Name string
+	Age  uint
+}
+
+func main() {
+	db := newDB()
+
+	user1 := User{
+		Name: "John Doe",
+		Age:  42,
+	}
+	db.Insert(user1)
+
+	fmt.Println(db.Tables)
+
+	userX, ok := db.Get(0, User{}).(User)
+	if !ok {
+		return
+	}
+
+	userX.Age = 43
+	fmt.Println(user1)
+	fmt.Println(userX)
+
+	db.Update(0, userX)
+	fmt.Println(db.Tables)
+	fmt.Println(db.Get(0, User{}))
+}
