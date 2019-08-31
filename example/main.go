b18911fc3209aa979752938cc681a17689067aaa
lmuench <muench.ludwig@gmail.com>
2019-08-31 09:16:37 -0400

    move example main to example package

---
+++ example/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/lmuench/gobdb/example/model"
+	"github.com/lmuench/gobdb/gobdb"
+)
+
+// Example gobdb usage
+func main() {
+	db := &gobdb.DB{Path: "/tmp/gobdb/example_dev"}
+
+	u1 := model.User{
+		Name: "John Doe",
+		Age:  42,
+	}
+	db.Insert(&u1)
+
+	tdl1 := model.TodoList{
+		UserID: u1.GetID(),
+		Title:  "My Todo List",
+	}
+	db.Insert(&tdl1)
+
+	var tdlX1 model.TodoList
+	_ = db.Get(&tdlX1, tdl1.GetID())
+	fmt.Println(tdlX1)
+
+	uX1, err := tdlX1.GetUser(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(uX1)
+
+	todoLists, err := model.GetAllTodoLists(db)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(todoLists)
+}
